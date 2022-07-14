78d1473fa8e4efea743caef361c5813208141f8c
Hanna Bacelar <hannabacelar@hotmail.com>
2022-07-14 20:27:15 -0300

    :lipstick: feat: criando model category

---
+++ src/database/models/category.js
@@ -0,0 +1,19 @@
+'use strict';
+
+  const createUserModel = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: DataTypes.STRING,
+    }, {
+      underscored: true,
+      timestamps: false,
+      tableName: 'Categories'
+    })
+    return Category;
+  };
+  module.exports =  createUserModel;
---
+++ src/services /user.service.js
@@ -0,0 +1,13 @@
+// const Sequelize = require('sequelize');
+// const config = require('../database/config/config');
+
+// const sequelize = new Sequelize(config.development);
+// const Joi = require('joi');
+
+// const userModel = require('../database/models/user');
+
+// const creatUser = async () => {
+//     const result = await userModel.create({});
+//     return result;
+// };
+// module.exports = { creatUser };
